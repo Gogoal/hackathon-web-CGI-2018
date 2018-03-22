7d14eeabd922756361f52ff1cad306c075c392f8
Mignot, Samuel <samuel.mignot@cgi.com>
2018-03-22 16:20:07 +0100

    Add news page

--- src/app/news/component/list/list.component.ts
+++ src/app/news/component/list/list.component.ts
@@ -7,8 +7,53 @@ import { Component } from '@angular/core';
 })
 
 export class ListComponent {
+  constructor() {}
 
-    constructor() {
+  articles = [{
+    title: "DBZ",
+    date: "LE 22 MARS 2018",
+    picture: "une photo badass",
+    summary: "Lorem ipsum",
+    visible: true
+  }, {
+    title: "Witcher",
+    date: "LE 1000",
+    picture: "une autre photo badass",
+    summary: "Lorem ipsum",
+    visible: true
+  }, {
+    title: "TEST YOLO",
+    date: "LE 1000",
+    picture: "une autre photo badass",
+    summary: "Lorem ipsum",
+    visible: true
+  }];
 
+  SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
+
+  // action triggered when user swipes
+  swipe(currentIndex: number, action = this.SWIPE_ACTION.RIGHT) {
+    // out of range
+    if (currentIndex > this.articles.length || currentIndex < 0) return;
+
+    let nextIndex = 0;
+
+    // swipe right =>  j'aime le profil
+    if (action === this.SWIPE_ACTION.RIGHT) {
+      // const isLast = currentIndex === this.articles.length - 1;
+      // nextIndex = isLast ? 0 : currentIndex + 1;
+      alert("J'aime !");
     }
+
+    // swipe left => je n'aime pas le profil
+    if (action === this.SWIPE_ACTION.LEFT) {
+      // const isFirst = currentIndex === 0;
+      // nextIndex = isFirst ? this.articles.length - 1 : currentIndex - 1;
+      this.articles[currentIndex].visible = false;
+    }
+
+    // toggle article visibility
+    // this.articles.forEach((x, i) => x.visible = (i === nextIndex));
+  }
+
 }
