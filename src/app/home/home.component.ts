2263d72615de8228f844935f2b186869ae58fc69
Mignot, Samuel <samuel.mignot@cgi.com>
2018-03-23 01:27:38 +0100

    add easter eggs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,22 +18,74 @@ export class HomeComponent {
 
   public ivg = 'assets/img/cat_ivg.png';
   public fav = 'assets/img/cat_favoris.jpg';
+  public counter : number = 0;
+  public opacity : number = 1;
+  public boolBandai : boolean = false;
+  public boolSheronGif : boolean = false;
+  public boolSheronWish : boolean = false;
 
-    constructor(private newsServ: NewsService, public dialog: MatDialog) {
+  constructor(private newsServ: NewsService, public dialog: MatDialog) {
 
-      this.newsServ.getNews().subscribe(
-        data => {
-          console.log(data);
-        },
-        err => {
+    this.newsServ.getNews().subscribe(
+      data => {
+        console.log(data);
+       },
+      err => {
           console.log(err);
-        }
-      );
+      }
+    );
 
-    }
+  }
+
 
   goHome() {
     //this.router.navigate(['news']);
+    this.counter += 1;
+    console.log(this.counter);
+    if(this.counter === 3) {
+      alert("Tu as trouvé 3 dragons balls !");
+    }
+  }
+
+  goLanguage() {
+    //this.router.navigate(['news']);
+    this.counter += 1;
+    console.log(this.counter);
+    if(this.counter === 5) {
+      alert("Tu as trouvé 5 dragons balls !");
+    }
+  }
+
+  goBookmarks() {
+    //this.router.navigate(['news']);
+    this.counter += 1;
+    console.log(this.counter);
+    if(this.counter === 6) {
+      alert("Tu as trouvé 6 dragons balls !");
+    }
+  }
+
+  goAutorenew() {
+    //this.router.navigate(['news']);
+    this.counter += 1;
+    console.log(this.counter);
+    if(this.counter === 7) {
+      this.boolBandai = true;
+
+      setTimeout(function() {
+        this.boolBandai = false;
+        this.boolSheronGif = true;
+      }.bind(this), 3000);
+
+      setTimeout(function() {
+        this.boolSheronGif = false;
+        this.boolSheronWish = true;
+      }.bind(this), 19000);
+
+      setTimeout(function() {
+        this.boolSheronWish = false;
+      }.bind(this), 30000);
+    }
   }
 
   pickDataUser1() {
